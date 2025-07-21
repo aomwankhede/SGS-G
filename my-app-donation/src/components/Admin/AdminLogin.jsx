84dbda43ba7eb83bb17587c702620016bccb1d2c
Arun <arungagare2915@gmail.com>
2025-07-21 07:14:48 +0530

    added render url to login

--- my-app-donation/src/components/Admin/AdminLogin.jsx
+++ my-app-donation/src/components/Admin/AdminLogin.jsx
@@ -22,7 +22,7 @@ const AdminLogin = () => {
  const handleLogin = async (e) => {
   e.preventDefault();
   try {
-    const response = await fetch('http://localhost:5000/api/admin/login', {
+    const response = await fetch('https://sgs-2jrp.onrender.com/api/admin/login', {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
